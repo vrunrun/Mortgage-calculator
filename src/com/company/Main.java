e24cd748a8c8c24b24a00c533b7088bc99381e85
kl <kl@debian>
2020-01-07 12:20:44 +0800

    Add input validations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,27 +7,51 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // Avoid magic numbers in code
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+  public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+    // Constants
+    final byte MONTHS_IN_YEAR = 12;
+    final byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int principal = (int) scanner.nextInt();
+    int principal = 0;
+    float monthlyInterestRate = 0F;
+    short numberOfPayments = 0;
 
-        System.out.print("Annual Interest Rate: ");
-        float monthlyInterestRate = scanner.nextFloat() / MONTHS_IN_YEAR / PERCENT;
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Period (Years): ");
-        short numberOfPayment = (short) (scanner.nextShort() * MONTHS_IN_YEAR);
-
-        // Formula source: https://www.wikihow.com/Calculate-Mortgage-Payments
-        double monthlyMortgageAmount = (principal *
-                monthlyInterestRate * Math.pow(1+monthlyInterestRate, numberOfPayment) /
-                (Math.pow(1+monthlyInterestRate, numberOfPayment) - 1));
-
-        System.out.println("Mortgage " + NumberFormat.getCurrencyInstance().format(monthlyMortgageAmount));
+    while(true){
+      System.out.print("Principal ($1K - $1M): ");
+      principal = (int) scanner.nextInt();
+      if (principal >= 1000 && principal <= 1_000_000)
+        break;
+      System.out.println("Enter a number between 1,000 and 1,000,000");
     }
+
+    while (true) {
+      System.out.print("Annual Interest Rate (1% - 30%): ");
+      float annualInterestRate = scanner.nextFloat();
+      if (annualInterestRate > 1 && annualInterestRate < 30) {
+        monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+        break;
+      }
+      System.out.println("Enter a number between 1 and 30");
+    }
+
+    while(true) {
+      System.out.print("Period (Years) (1 - 30): ");
+      short numberOfYears = (short) scanner.nextShort();
+      if (numberOfYears >= 1 && numberOfYears <= 30) {
+        numberOfPayments = (short) (numberOfYears * MONTHS_IN_YEAR);
+        break;
+      }
+      System.out.println("Enter a number between 1 and 30");
+    }
+
+    // Formula source: https://www.wikihow.com/Calculate-Mortgage-Payments
+    double monthlyMortgageAmount = (principal *
+        monthlyInterestRate * Math.pow(1+monthlyInterestRate, numberOfPayments) /
+        (Math.pow(1+monthlyInterestRate, numberOfPayments) - 1));
+
+    System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(monthlyMortgageAmount));
+  }
 }
