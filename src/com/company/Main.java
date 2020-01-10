9b3fefab6d2e4cd9b3f0cbb134233fe2f4982708
kl <kl@debian>
2020-01-10 16:27:39 +0800

    Refactor remove repetitive while loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,40 +8,27 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-
-    int principal = 0;
-    short numberOfYears = 0;
-    float annualInterestRate = 0;
-
-    Scanner scanner = new Scanner(System.in);
-
-    while(true){
-      System.out.print("Principal ($1K - $1M): ");
-      principal = (int) scanner.nextInt();
-      if (principal >= 1000 && principal <= 1_000_000)
-        break;
-      System.out.println("Enter a number between 1,000 and 1,000,000");
-    }
-
-    while (true) {
-      System.out.print("Annual Interest Rate (1% - 30%): ");
-      annualInterestRate = scanner.nextFloat();
-      if (annualInterestRate > 1 && annualInterestRate < 30)
-        break;
-      System.out.println("Enter a number between 1 and 30");
-    }
-
-    while(true) {
-      System.out.print("Period (Years) (1 - 30): ");
-      numberOfYears = (short) scanner.nextShort();
-      if (numberOfYears >= 1 && numberOfYears <= 30)
-        break;
-
-      System.out.println("Enter a number between 1 and 30");
-    }
+    int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+    float annualInterestRate = (float) readNumber("Annual Interest Rate (%): ", 1, 30);
+    short numberOfYears = (short) readNumber("Period (Years): ", 1, 30);
 
     double mortgage = calculateMortgage(principal, annualInterestRate, numberOfYears);
+
     System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+  }
+
+  public static double readNumber(String prompt, double min, double max) {
+    Scanner scanner = new Scanner(System.in);
+    double value;
+
+    while(true){
+      System.out.print(prompt);
+      value = scanner.nextDouble();
+      if (value >= min && value <= max)
+        break;
+      System.out.println("Enter a number between " + min + " and " + max);
+    }
+    return value;
   }
 
   public static double calculateMortgage(
